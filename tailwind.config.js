60e46321a1ab9bf378080c4af55f3418287edc5c
Vanya <vtochevska@gmail.com>
2024-10-18 15:27:21 +0300

    added tailwind and styles

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{html,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    fontFamily: {
+      'sans': ['Helvetica', 'Arial', 'sans-serif'],
+    }
+  },
+  plugins: [
+  ],
+}
