0333331c1933f6c52af743db88be0e7aed77e79d
Vanya <vtochevska@gmail.com>
2024-10-18 14:55:17 +0300

    added app files, requirements and gitignore

---
+++ app.py
@@ -0,0 +1,121 @@
+from flask import Flask, render_template, request, redirect, url_for
+import psycopg2
+
+app = Flask(__name__)
+
+conn = psycopg2.connect(database="flask_db", user="",
+                        password="", host="localhost", port="5432")
+print('connected')
+cur = conn.cursor()
+
+# if you already have any table or not id doesnt matter this
+# will create a products table for you.
+cur.execute(
+    '''CREATE TABLE IF NOT EXISTS products (id serial PRIMARY KEY, name varchar(100), price float);''')
+
+# commit the changes
+conn.commit()
+
+# close the cursor and connection
+cur.close()
+conn.close()
+
+
+@app.route('/')
+def index():
+    # Connect to the database
+    conn = psycopg2.connect(database="flask_db", user="",
+                            password="", host="localhost", port="5432")
+
+    # create a cursor
+    cur = conn.cursor()
+
+    # Select all products from the table
+    cur.execute('''SELECT * FROM products''')
+
+    # Fetch the data
+    data = cur.fetchall()
+
+    # close the cursor and connection
+    cur.close()
+    conn.close()
+    print('Hello')
+    return render_template('index.html', data=data)
+
+
+@app.route('/create', methods=['POST'])
+def create():
+    conn = psycopg2.connect(database="flask_db", user="",
+                            password="", host="localhost", port="5432")
+
+    print('connected')
+
+    cur = conn.cursor()
+
+    # Get the data from the form
+    name = request.form['name']
+    price = request.form['price']
+
+    # Insert the data into the table
+    cur.execute(
+        '''INSERT INTO products (name, price) VALUES (%s, %s)''', (name, price))
+
+    # commit the changes
+    conn.commit()
+
+    # close the cursor and connection
+    cur.close()
+    conn.close()
+
+    return redirect(url_for('index'))
+
+
+@app.route('/update', methods=['POST'])
+def update():
+    conn = psycopg2.connect(database="flask_db",
+                            user="",
+                            password="",
+                            host="localhost", port="5432")
+
+    cur = conn.cursor()
+
+    # Get the data from the form
+    name = request.form['name']
+    price = request.form['price']
+    id = request.form['id']
+
+    # Update the data in the table
+    cur.execute(
+        '''UPDATE products SET name=%s, price=%s WHERE id=%s''', (name, price, id))
+
+    # commit the changes
+    conn.commit()
+    return redirect(url_for('index'))
+
+
+@app.route('/delete', methods=['POST'])
+def delete():
+    conn = psycopg2.connect(database="flask_db",
+                            user="",
+                            password="",
+                            host="localhost", port="5432")
+
+    cur = conn.cursor()
+    # Get the data from the form
+    id = request.form['id']
+
+    # Delete the data from the table
+    cur.execute('''DELETE FROM products WHERE id=%s''', (id,))
+
+    # commit the changes
+    conn.commit()
+
+    # close the cursor and connection
+    cur.close()
+    conn.close()
+
+    return redirect(url_for('index'))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
